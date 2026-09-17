059980eab5cc97ab5962a21bb20bdf6b5ae951c7
Daviiiiiiiiiiid <amir.davodpour@gmail.com>
2026-09-17 16:24:55 +0330

    [CI-FIX] restore job detail viewport contracts

--- test/features/marketplace/job_detail_page_test.dart
+++ test/features/marketplace/job_detail_page_test.dart
@@ -227,10 +227,6 @@ void main() {
   testWidgets('mission details render pricing, duration and transaction entry',
       (tester) async {
     await _pump(tester, job: _job());
-    final scrollable =
-        tester.state<ScrollableState>(find.byType(Scrollable).first);
-    scrollable.position.jumpTo(scrollable.position.maxScrollExtent);
-    await tester.pumpAndSettle();
     expect(find.text('Mission details'), findsOneWidget);
     expect(
       tester.widget<JobDetailPage>(find.byType(JobDetailPage)).job.title,
@@ -246,10 +242,6 @@ void main() {
   testWidgets('job details render monthly pay, deadline and admin banner',
       (tester) async {
     await _pump(tester, job: _job(kind: 'JOB'));
-    final scrollable =
-        tester.state<ScrollableState>(find.byType(Scrollable).first);
-    scrollable.position.jumpTo(scrollable.position.maxScrollExtent);
-    await tester.pumpAndSettle();
     expect(find.text('Job details'), findsOneWidget);
     expect(
       tester.widget<JobDetailPage>(find.byType(JobDetailPage)).job.title,
@@ -283,10 +275,7 @@ void main() {
       userId: 'u1',
     );
 
-    final scrollable =
-        tester.state<ScrollableState>(find.byType(Scrollable).first);
-    scrollable.position.jumpTo(scrollable.position.maxScrollExtent);
-    await tester.pumpAndSettle();
+    await tester.ensureVisible(find.text('Forwarded candidates'));
     expect(find.text('Forwarded candidates'), findsOneWidget);
     expect(find.text('Anonymous candidate'), findsNWidgets(2));
     expect(find.text('Flutter'), findsOneWidget);
