c8b7d8af31feabc86d2060d5d30d4835a54b5798
Daviiiiiiiiiiid <amir.davodpour@gmail.com>
2026-09-20 22:45:59 +0330

    ui: align login actions for RTL

--- lib/features/auth/login_page.dart
+++ lib/features/auth/login_page.dart
@@ -200,7 +200,7 @@ class _LoginPageState extends State<LoginPage> {
                         ),
                       ),
                       Align(
-                        alignment: Alignment.centerRight,
+                        alignment: AlignmentDirectional.centerEnd,
                         child: TextButton(
                           onPressed: () => Navigator.push(
                               context, HopeRoutes.passwordReset()),
