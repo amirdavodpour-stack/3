8f4fb3d12f5a368e700ec6ad7605849ba5cd9b31
Daviiiiiiiiiiid <amir.davodpour@gmail.com>
2026-09-20 21:36:27 +0330

    test: update auth repository fake for Google sign-in

--- test/features/profile/profile_page_test.dart
+++ test/features/profile/profile_page_test.dart
@@ -15,6 +15,9 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class _AuthRepo implements AuthRepository {
   @override
+  Future<AuthSession> loginWithGoogle(String _) =>
+      throw UnimplementedError();
+  @override
   Future<AuthSession> login(String e, String p) => throw UnimplementedError();
   @override
   Future<AuthSession> register(String e, String p, String n) =>
