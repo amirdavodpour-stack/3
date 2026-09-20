1c1d153bc5a49e198fa4e83bd5abee2f95559a8b
Daviiiiiiiiiiid <amir.davodpour@gmail.com>
2026-09-21 03:10:04 +0330

    test: require wordmark on wide home rail

--- test/features/home/home_page_test.dart
+++ test/features/home/home_page_test.dart
@@ -12,6 +12,7 @@ import 'package:hope_mobile/core/theme/theme_controller.dart';
 import 'package:hope_mobile/core/transactions/transaction_repository.dart';
 import 'package:hope_mobile/features/home/home_page.dart';
 import 'package:hope_mobile/core/ui/premium_components.dart';
+import 'package:hope_mobile/core/branding/widgets/hope_logo.dart';
 import 'package:hope_mobile/l10n/generated/app_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -114,6 +115,21 @@ void main() {
     await tester.pumpAndSettle();
 
     expect(find.byType(PremiumNavigationRail), findsOneWidget);
+
+    tester.view.resetPhysicalSize();
+    tester.view.resetDevicePixelRatio();
+  });
+
+  testWidgets('wide desktop rail exposes the HOPE wordmark',
+      (tester) async {
+    tester.view.physicalSize = const Size(1280, 800);
+    tester.view.devicePixelRatio = 1;
+
+    await tester.pumpWidget(await _app());
+    await tester.pumpAndSettle();
+
+    final logo = tester.widget<HopeLogo>(find.byType(HopeLogo));
+    expect(logo.showWordmark, isTrue);
 
     tester.view.resetPhysicalSize();
     tester.view.resetDevicePixelRatio();
