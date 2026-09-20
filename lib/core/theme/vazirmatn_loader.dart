512b71d570d1b30ed09d0af1d5e109642f7fe49b
Daviiiiiiiiiiid <amir.davodpour@gmail.com>
2026-09-20 22:05:28 +0330

    feat(typography): add local Vazirmatn loader

---
+++ lib/core/theme/vazirmatn_loader.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:flutter/services.dart';
+
+const _vazirmatnAsset = 'assets/fonts/vazirmatn_wght.ttf.b64';
+
+Future<void> loadVazirmatnFont() async {
+  final encoded = await rootBundle.loadString(_vazirmatnAsset);
+  final bytes = base64Decode(encoded);
+  final loader = FontLoader('Vazirmatn');
+  loader.addFont(Future<ByteData>.value(ByteData.view(bytes.buffer)));
+  await loader.load();
+}
