74cfb8bb89598fda789d2192c05328ebc5356811
Daviiiiiiiiiiid <amir.davodpour@gmail.com>
2026-09-22 15:24:25 +0330

    chore: remove unused theme import from lifecycle UI

--- lib/core/ui/premium_lifecycle.dart
+++ lib/core/ui/premium_lifecycle.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../theme/hope_v2_design.dart';
-import '../theme/app_theme.dart';
 import 'premium_components.dart';
 
 class PremiumLifecycleStep {
