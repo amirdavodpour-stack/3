5a1488d4316906ccb644eec7df1de5a8c7d98bf3
Daviiiiiiiiiiid <amir.davodpour@gmail.com>
2026-09-23 04:56:00 +0330

    test(a11y): use platform clear tooltip contract

--- test/core/ui/components_interaction_test.dart
+++ test/core/ui/components_interaction_test.dart
@@ -22,15 +22,15 @@ void main() {
       ),
     );
 
-    expect(find.byTooltip('پاک کردن جست‌وجو'), findsNothing);
+    expect(find.byTooltip(MaterialLocalizations.of(tester.element(find.byType(TextField)).context).clearButtonTooltip), findsNothing);
 
     await tester.enterText(find.byType(TextField), 'کار');
     await tester.pump();
 
     expect(values.last, 'کار');
-    expect(find.byTooltip('پاک کردن جست‌وجو'), findsOneWidget);
+    expect(find.byTooltip(MaterialLocalizations.of(tester.element(find.byType(TextField)).context).clearButtonTooltip), findsOneWidget);
 
-    await tester.tap(find.byTooltip('پاک کردن جست‌وجو'));
+    await tester.tap(find.byTooltip(MaterialLocalizations.of(tester.element(find.byType(TextField)).context).clearButtonTooltip));
     await tester.pump();
 
     expect(find.byType(TextField), findsOneWidget);
