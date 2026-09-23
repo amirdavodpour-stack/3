7bba1223d5794b9ec9daafc0cdb5203d888f680c
Daviiiiiiiiiiid <amir.davodpour@gmail.com>
2026-09-23 20:37:38 +0330

    chore: remove redundant shell theme locals

--- lib/core/ui/premium_components.dart
+++ lib/core/ui/premium_components.dart
@@ -19,7 +19,6 @@ class PremiumNavigationBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final scheme = Theme.of(context).colorScheme;
     final dark = Theme.of(context).brightness == Brightness.dark;
     return DecoratedBox(
       decoration: BoxDecoration(
@@ -67,7 +66,6 @@ class PremiumNavigationRail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final scheme = Theme.of(context).colorScheme;
     return DecoratedBox(
       decoration: BoxDecoration(
         color: HopeV2Surfaces.navigation(context),
@@ -698,28 +696,3 @@ class PremiumSearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Semantics(
         textField: true,
-        label: hint,
-        child: ConstrainedBox(
-          constraints: const BoxConstraints(minHeight: HopeV2Touch.minimum),
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(HopeV2Radii.lg),
-              boxShadow: Theme.of(context).brightness == Brightness.dark
-                  ? const []
-                  : const [
-                      BoxShadow(
-                        color: Color(0x081B1638),
-                        blurRadius: 18,
-                        offset: Offset(0, 8),
-                      ),
-                    ],
-            ),
-            child: SearchField(
-              onChanged: onChanged,
-              onFilter: onFilter,
-              hint: hint,
-            ),
-          ),
-        ),
-      );
-}
