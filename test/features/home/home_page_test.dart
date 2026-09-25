12f349bd6af81e86c38c318d8eae9e9102b6b61f
Daviiiiiiiiiiid <amir.davodpour@gmail.com>
2026-09-25 14:53:28 +0330

    test(ui): retain shared premium component imports

--- test/features/home/home_page_test.dart
+++ test/features/home/home_page_test.dart
@@ -11,6 +11,7 @@ import 'package:hope_mobile/core/storage/secure_store.dart';
 import 'package:hope_mobile/core/theme/theme_controller.dart';
 import 'package:hope_mobile/core/transactions/transaction_repository.dart';
 import 'package:hope_mobile/features/home/home_page.dart';
+import 'package:hope_mobile/core/ui/premium_components.dart';
 import 'package:hope_mobile/core/branding/widgets/hope_logo.dart';
 import 'package:hope_mobile/l10n/generated/app_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
