731b57948af0185492e1443c869cf1642d2f3e27
Daviiiiiiiiiiid <amir.davodpour@gmail.com>
2026-09-26 05:14:42 +0330

    fix(runtime): import ValueListenable foundation API

--- integration_test/runtime/critical_screens_evidence_test.dart
+++ integration_test/runtime/critical_screens_evidence_test.dart
@@ -2,6 +2,7 @@
 
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
